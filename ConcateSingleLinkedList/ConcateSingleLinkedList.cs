88cd94e178e22ebcf21355d91c7f6361fcde321b
Srujana-Jami <srujana.jami@innroad.com>
2020-07-31 12:14:47 +0530

    Add project files.

---
+++ ConcateSingleLinkedList/ConcateSingleLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConcateSingleLinkedList
+{
+    class ConcateSingleLinkedList
+    {
+        private Node start;
+        public ConcateSingleLinkedList()
+        {
+            start = null;
+        }
+        public void CreateList()
+        {
+            int i, n, data;
+            Console.Write("Enter the number of nodes");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("Enter the element to be inserted: ");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(data);
+            }
+        }
+        public void DisplayList()
+        {
+            Node p;
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Console.Write("List is : ");
+            p = start;
+            while (p != null)
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+        public void InsertAtEnd(int data)
+        {
+            Node p;
+            Node temp = new Node(data);
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+            p = start;
+            while (p.link != null)
+            {
+                p = p.link;
+
+            }
+            p.link = temp;
+        }
+
+        public void Concate(ConcateSingleLinkedList list)
+        {
+          if(start == null)
+            {
+                start = list.start;
+                return;
+            }
+            if (list.start == null)
+                return;
+            Node p = start;
+            while (p.link != null)
+                p = p.link;
+            p.link = list.start;
+        }
+
+        
+
+        
+
+    }
+}
---
+++ ConcateSingleLinkedList/Demo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcateSingleLinkedList
+{
+    class Demo
+    {
+    static void Main(string[] args)
+        {
+            ConcateSingleLinkedList list1 = new ConcateSingleLinkedList();
+            ConcateSingleLinkedList list2 = new ConcateSingleLinkedList();
+
+            Console.WriteLine(" Enter first list : ");
+            list1.CreateList();
+            Console.WriteLine(" Enter second list : ");
+            list2.CreateList();
+
+            Console.Write(" First ");
+            list1.DisplayList();
+            Console.Write("Second ");
+            list2.DisplayList();
+
+            list1.Concate(list2);
+            Console.Write(" First ");
+            list1.DisplayList();
+        }
+    
+    
+    }
+}
